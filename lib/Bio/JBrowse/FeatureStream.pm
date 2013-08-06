422250974ebbf5874ac6fd35dd894ee04e1842a8
Robert Buels <rmb32@cornell.edu>
2013-08-06 12:47:25 -0400

    update from JBrowse core

--- lib/Bio/JBrowse/FeatureStream.pm
+++ lib/Bio/JBrowse/FeatureStream.pm
@@ -2,6 +2,9 @@ package Bio::JBrowse::FeatureStream;
 use strict;
 use warnings;
 
+my %must_flatten =
+   map { $_ => 1 }
+   qw( name id start end score strand description note );
 # given a hashref like {  tagname => [ value1, value2 ], ... }
 # flatten it to numbered tagnames like { tagname => value1, tagname2 => value2 }
 sub _flatten_multivalues {
@@ -10,8 +13,15 @@ sub _flatten_multivalues {
 
     for my $key ( keys %$h ) {
         my $v = $h->{$key};
-        for( my $i = 0; $i < @$v; $i++ ) {
-            $flattened{ $key.($i ? $i+1 : '')} = $v->[$i];
+        if( @$v == 1 ) {
+            $flattened{ $key } = $v->[0];
+        }
+        elsif( $must_flatten{ lc $key } ) {
+            for( my $i = 0; $i < @$v; $i++ ) {
+                $flattened{ $key.($i ? $i+1 : '')} = $v->[$i];
+            }
+        } else {
+            $flattened{ $key } = $v;
         }
     }
 
